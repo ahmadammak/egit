d6c8417cc4ac790cdfcd2c085543f43430d30742
ahmadammak <ahmad_ammak@yahoo.com>
2015-01-03 23:04:04 +0200

    2nd

--- egit/src/egit/egit.java
+++ egit/src/egit/egit.java
@@ -4,7 +4,7 @@ public class egit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ahmad majed abu khalil");
+		System.out.println("ahmad majed ");
 
 	}
 
